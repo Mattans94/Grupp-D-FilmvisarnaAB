affc7373daf7b9835006f3f52b97a25308214ff8
Unknown <paulina.hertor@gmail.com>
2018-01-17 11:34:58 +0100

    Script made into Class Showtime and saves to JSON

--- www/js/showscript.js
+++ www/js/showscript.js
@@ -1,50 +1,58 @@
-let movieNames = ['Let The Sunshine In', 'Wind River', 'Three Billboards Outside Ebbing, Missouri', 'Tjuren Ferdinand', 'Call Me By Your Name'];
-let theater = ['Stora Salongen', 'Lilla Salongen'];
-let times = ['18.40', '21.00', '22.40'];
-let myArrayOfObjects = [];
+class Showtime{
 
-function randomMovie() {
-  let rnd = Math.floor(Math.random() * 5);
-  return movieNames[rnd];
-}
+  constructor(){
+    this.movieNames = ['Let The Sunshine In', 'Wind River', 'Three Billboards Outside Ebbing, Missouri', 'Tjuren Ferdinand', 'Call Me By Your Name'];
+    this.theater = ['Stora Salongen', 'Lilla Salongen'];
+    this.times = ['18.40', '21.00', '22.40'];
+    this.myArrayOfObjects = [];
+    this.randomWriteObjectsToArray();
+    this.saveToJSON(this.myArrayOfObjects);
+      }
 
-function randomTheater() {
-  let rnd = Math.floor(Math.random() * 2);
-  return theater[rnd];
-}
+  randomMovie() {
+    let rnd = Math.floor(Math.random() * 5);
+    return this.movieNames[rnd];
+  }
 
-function randomWriteShowsToJSON() {
-  let currentDate;
-  let myDateCo = 1;
+  randomTheater() {
+    let rnd = Math.floor(Math.random() * 2);
+    return this.theater[rnd];
+  }
+
+  randomWriteObjectsToArray() {
+    let currentDate;
+    let myDateCo = 1;
 
 
-  for (let j = 0; j < 28; j++){
-    if (myDateCo < 10) {
-      currentDate = '2018-03-0'+myDateCo;
-    } else {
-      currentDate = '2018-03-'+myDateCo;
-    }
-    myDateCo++;
+    for (let j = 0; j < 28; j++){
+      if (myDateCo < 10) {
+        currentDate = '2018-03-0'+myDateCo;
+      } else {
+        currentDate = '2018-03-'+myDateCo;
+      }
+      myDateCo++;
 
-    for (let i = 0; i < 3; i++) {
-      let tempObject = {};
-      tempObject.date = currentDate;
-      tempObject.auditorium = randomTheater();
-      tempObject.film = randomMovie();
-      tempObject.time = times[i];
+      for (let i = 0; i < 3; i++) {
+        let tempObject = {};
+        tempObject.date = currentDate;
+        tempObject.auditorium = this.randomTheater();
+        tempObject.film = this.randomMovie();
+        tempObject.time = this.times[i];
 
-      myArrayOfObjects.push(tempObject);
-      console.log(tempObject);
+        this.myArrayOfObjects.push(tempObject);
+      }
     }
   }
+
+
+  saveToJSON(object){
+    JSON._save('shows.json', {
+        Shows: object
+    });
+  }
+
 }
 
-function addToArray(object){
 
-}
 
-function saveToJSON(object){
-  JSON._save('shows.json', {
-      Shows: object
-  });
-}
+
