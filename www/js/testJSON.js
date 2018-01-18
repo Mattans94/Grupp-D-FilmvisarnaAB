e19eef02322a1919393e34a6c7a9498d7437067a
Unknown <andreas@stickydirt.se>
2018-01-18 12:31:14 +0100

    Started with rendering out JSON.

--- www/js/testJSON.js
+++ www/js/testJSON.js
@@ -1,39 +1,50 @@
 class GetJSON {
   constructor(){
-    this.myJSON;
-    this.getJSONFromMovies();
-    this.firstObject;
-
+    this.movieObjects;
+    this.showObjects;
+    this.getJSONForMovies();
+    this.getJSONForShows();
   }
 
-  getJSONFromMovies() {
-    JSON._load('movies')
+  getJSONForShows() {
+    JSON._load('shows')
     .then((data) => {
       // Retrieve the app from JSON
-      this.myJSON = data;
+      this.showObjects = data;
     })
     .catch((errorMessage) => {
     	console.log(errorMessage);
     })
   }
 
-  consoleMe() {
-    console.log(this.myJSON[0].title);
+  getJSONForMovies() {
+    JSON._load('movies')
+    .then((data) => {
+      // Retrieve the app from JSON
+      this.movieObjects = data;
+    })
+    .catch((errorMessage) => {
+    	console.log(errorMessage);
+    })
   }
 
   checkMovie(movieTitle) {
     // Example 1:
-    // return this.myJSON.find(function(m) {
+    // return this.movieObjects.find(function(m) {
     //   return movieTitle == m.title;
     // })
     // Example 2:
-    // for (let i = 0; i < this.myJSON.length; i++){
-    //   if (movieTitle == this.myJSON[i].title) {
-    //     return this.myJSON[i];
+    // for (let i = 0; i < this.movieObjects.length; i++){
+    //   if (movieTitle == this.movieObjects[i].title) {
+    //     return this.movieObjects[i];
     //   }
     // }
     // Example 3
-    return this.myJSON.find((m) => movieTitle == m.title)
+    return this.movieObjects.find((m) => movieTitle == m.title)
+  }
+
+  checkShows(movieTitle) {
+    return this.showObjects.filter((m) => movieTitle == m.film);
   }
 
   getActors(movieObject){
@@ -44,18 +55,24 @@ class GetJSON {
   	return actors;
  }
 
+  renderShowtime(movieObject){
+   let allShows = this.checkShows(movieObject.title);
+   console.log(allShows);
+   setTimeout(()=>{console.log(allShows)},1000)
+   return `<li>Var: ${allShows[0].auditorium} Datum: ${allShows[0].date} Tid: ${allShows[0].time}`
+  }
+
   renderDescription(movieTitle) {
     let movieObject = this.checkMovie(movieTitle);
-    console.log(movieObject.youtubeTrailers)
     $('main').html(`
-            
+
         <article class="row">
 
           <section class="col-lg-7 d-flex flex-wrap d-md-block bg-dark mt-5 pb-3">
-            
+
 
             <div class="embed-responsive embed-responsive-16by9 mt-3">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${movieObject.youtubeTrailers}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${movieObject.youtubeTrailers[0]}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
             </div>
 
 
@@ -70,12 +87,9 @@ class GetJSON {
 
                   <div id="collapseOne" class="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
                     <div class="p-3">
-                      <li><strong>Land: </strong>Frankrike</li>
-                      <li><strong>Produktionsår: </strong>2017</li>
-                      <li><strong>Längd: </strong>94 min</li>
-                      <li><strong>Genre: </strong>Drama</li>
-                      <li><strong>Språk: </strong>Franska</li>
-                      <li><strong>Textning: </strong>Svenska</li>
+                      <ul>
+                        ${this.renderShowtime(movieObject)}
+                      </ul>
                     </div>
                   </div>
                 </div>
@@ -96,9 +110,8 @@ class GetJSON {
                   <div class="col-md-12 p-3 pt-0">
                     <h2>${movieObject.title}</h2>
                     <p>${movieObject.description}</p>
-                    <p>Invigningsfilm av kritikerveckan och vinnare av SACD Award i Cannes 2017.</p>
 
-                    <p><strong>${movieObject.reviews[0].quote}</strong><em>${movieObject.reviews[0].source}</em></p>
+                    <p><strong>&ldquo;${movieObject.reviews[0].quote}&ldquo;</strong><em>- ${movieObject.reviews[0].source}</em></p>
                     <p><strong>Medverkande:</strong>${this.getActors(movieObject)}</p>
                   </div>
               </div>
