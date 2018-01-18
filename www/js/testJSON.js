8a365fa93d799c24545c3c1779e9ff81f281b8be
Unknown <paulina.hertor@gmail.com>
2018-01-18 11:17:05 +0100

    Added moviedescription in json

--- www/js/testJSON.js
+++ www/js/testJSON.js
@@ -46,16 +46,16 @@ class GetJSON {
 
   renderDescription(movieTitle) {
     let movieObject = this.checkMovie(movieTitle);
-    $('.myContent').html(`
+    console.log(movieObject.youtubeTrailers)
+    $('main').html(`
             
         <article class="row">
 
           <section class="col-lg-7 d-flex flex-wrap d-md-block bg-dark mt-5 pb-3">
-            HEJHEJHEJHEJ
-
+            
 
             <div class="embed-responsive embed-responsive-16by9 mt-3">
-              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/6ZfuNTqbHE8?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${movieObject.youtubeTrailers}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
             </div>
 
 
@@ -84,23 +84,22 @@ class GetJSON {
                 <div class="d-flex flex-wrap d-md-block">
                   <div class="col-12 col-md-auto float-md-right m-0 m-md-3 p-0 order-2">
                     <ul class="p-3 info-box mb-0">
-                      <li><strong>Land: </strong>$(movieObject.productionCountries)</li>
-                      <li><strong>Produktionsår: </strong>$(movieObject.productionYear)</li>
-                      <li><strong>Längd: </strong>$(movieObject.length)</li>
-                      <li><strong>Genre: </strong>$(movieObject.genre)</li>
-                      <li><strong>Språk: </strong>$(movieObject.language)</li>
-                      <li><strong>Textning: </strong>$(movieObject.subtitles)</li>
+                      <li><strong>Land: </strong>${movieObject.productionCountries}</li>
+                      <li><strong>Produktionsår: </strong>${movieObject.productionYear}</li>
+                      <li><strong>Längd: </strong>${movieObject.length}</li>
+                      <li><strong>Genre: </strong>${movieObject.genre}</li>
+                      <li><strong>Språk: </strong>${movieObject.language}</li>
+                      <li><strong>Textning: </strong>${movieObject.subtitles}</li>
                     </ul>
                   </div>
 
                   <div class="col-md-12 p-3 pt-0">
-                    <h2>TITEL HÄR KANSKE LÅNG</h2>
-                    <p>Juliette Binoche är lysande i rollen som Isabelle, en frånskild konstnär i 50-års åldern, som försöker sålla bland alla självcentrerade män i hopp om att åter finna kärleken. En elegant romantisk komedi av kritikerrosade Claire Denis om vad som är viktigt i livet och om vad man vill ha i en mogen relation.</p>
+                    <h2>${movieObject.title}</h2>
+                    <p>${movieObject.description}</p>
                     <p>Invigningsfilm av kritikerveckan och vinnare av SACD Award i Cannes 2017.</p>
 
-                    <p><strong>"Mumma för skådespelare med kammarspelet i blodet" - </strong> <em>Sydsvenskan</em></p>
-                    <p><strong>"En regelrätt fest för biobesökarna" - </strong> <em>Svenska Dagbladet</em></p>
-                    <p><strong>Medverkande:</strong> Juliette Binoche, Gérard Depardieu, Valeria Bruni Tedeschi, Nicolas Duvauchelle, Claire Tran, Josiane Balasko, Xavier Beauvois, Alex Descas, Bruno Podalydès, Paul Blain</p>
+                    <p><strong>${movieObject.reviews[0].quote}</strong><em>${movieObject.reviews[0].source}</em></p>
+                    <p><strong>Medverkande:</strong>${this.getActors(movieObject)}</p>
                   </div>
               </div>
 
