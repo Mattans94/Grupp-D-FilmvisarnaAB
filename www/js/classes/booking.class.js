21c9d1d38ba8078999a807a5d54fab680fdee2f5
Unknown <paulina.hertor@gmail.com>
2018-01-24 10:12:41 +0100

    wip booking class

--- www/js/classes/booking.class.js
+++ www/js/classes/booking.class.js
@@ -1,9 +1,16 @@
 class Booking{
 
 	constructor(){
-		this. = 
-	}
-	
+		this.show = {
+	      "date": "2018-03-01",
+	      "auditorium": "Lilla Salongen",
+	      "film": "Tjuren Ferdinand",
+	      "time": "22.40"
+  		};
+
+  		this.userID = 1;
+  		this.seats = [];
+
 	}
 
 }
