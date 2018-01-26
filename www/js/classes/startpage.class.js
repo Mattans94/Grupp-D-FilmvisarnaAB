cca5c80fd4db02e9006c03de10ebf0cc05c66759
Unknown <andreas@stickydirt.se>
2018-01-26 10:24:32 +0100

    Designtweaks. Movies by themself on startpage.

--- www/js/classes/startpage.class.js
+++ www/js/classes/startpage.class.js
@@ -6,7 +6,7 @@ class StartPage extends Base {
 	renderMovie(movieTitle){
 		let movieObject = this.getMovieObject(movieTitle);
 		return `
-		<div class="col-6 col-md-6 movieBlock mb-3 d-flex flex-column">
+		<div class="movieBlock mb-3 mr-2 ml-2 p-3 d-flex flex-column">
 			<img class="img-fluid" src="/img/posters/${movieObject.images[0]}">
 
 			<h4 class="mt-2 mb-2">${movieObject.title}</h4>
