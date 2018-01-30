a66b39742c49f8016bd706cfe2d98ed78b8a3895
Johan <jmiklo@outlook.com>
2018-01-30 15:16:06 +0100

    created booking class

---
+++ www/js/classes/booking.class.js
@@ -0,0 +1,48 @@
+class Booking {
+
+	constructor() {
+
+	}
+
+	eventHandler() {
+		JSON._load('booking').then((seats) => {
+	      // Retrieve the app from JSON
+	      this.bookedSeats = seats;
+	      console.log(this.bookedSeats)
+	    });
+
+		this.booking = {};
+		let that = this;
+		$(document).on('click','.book-btn',function(){
+			
+			that.booking.show = {
+				"date": "2018-03-01",
+		      	"auditorium": "Lilla Salongen",
+		      	"film": "Tjuren Ferdinand",
+		      	"time": "22.40"
+			};
+
+			that.booking.show.userID = 1;
+			that.booking.show.seats = [];
+
+			console.log(this.booking);
+			$('.seat.booked').each(function(){
+				let seat = $(this);
+				let seatID = seat.data('seatid');
+				console.log(seatID);
+				that.booking.show.seats.push(seatID);
+			});
+
+			that.bookedSeats.push(that.booking);
+			//Save booked-info + sittplats to JSON
+			JSON._save('booking', that.bookedSeats);
+			console.log('saving', that.bookedSeats,)
+
+		});
+
+		// JSON._load('booking',(data){
+		// 	this.bookings = data;
+		// 	this.seatOccupied();
+		// });
+
+};
