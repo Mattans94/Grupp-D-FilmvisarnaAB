b485e1ac142c2536710e6866c6b123b744ab88c5
Johan <jmiklo@outlook.com>
2018-02-02 15:46:04 +0100

    booking class uploaded

---
+++ www/js/classes/booking.class.js
@@ -0,0 +1,153 @@
+class Booking extends Base{
+
+	// constructor(userid, date, time, auditorium) {
+	// 
+
+
+	constructor() {
+		super();
+		this.date = '2018-03-04';
+		this.time = '21.00';
+		this.auditorium = 'Lilla Salongen';
+		this.userID = 1;
+
+		this.child=0;
+		this.adult=0;
+		this.pensioner=0;
+	//Alla beställningar som har gjorts
+		JSON._load('booking').then((seats) => {
+	      // Retrieve the app from JSON
+	      this.bookedSeats = seats;
+	    });
+
+	    JSON._load('shows').then((shows) => {
+	      // Retrieve the app from JSON
+	      //console.log(this)
+	      this.showObjects = shows;
+	      this.start();
+	    });
+
+
+	}
+
+	start(){
+		this.getshowObject('Wind River', this.auditorium, this.date, this.time);
+	    this.eventHandler();
+	    this.renderTicketButtons();
+	    theater.scale();
+		
+	}
+
+	getshowObject(showName, auditorium, date, time) {
+		console.log(this.showObjects);
+		this.showObject = this.showObjects.object.filter((x) => showName == x.film);
+		this.showObject = this.showObject.filter((x) => auditorium == x.auditorium);
+		this.showObject = this.showObject.filter((x) => date == x.date);
+		this.showObject = this.showObject.find((x) => time == x.time);
+		// this.showObject = this.showObject[0]
+		console.log(this.showObject);
+	}
+
+	renderTicketButtons() {
+		let html = `<div class="row justify-content-center">
+			  <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+				  <div class="btn-group mr-2" role="group" aria-label="child group">
+				    <button type="button" class="btn btn-secondary adddbtn" id="addchild"><strong>+</strong></button>
+				    <input type="text" class="form-control col-2" id="childTickets" placeholder="${this.child}">
+				    <button type="button" class="btn btn-secondary removebtn" id="removechild"><strong>-</strong></button>
+				  </div>
+				</div>
+			  <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+				  <div class="btn-group mr-2" role="group" aria-label="adult group">
+				    <button type="button" class="btn btn-secondary addbtn" id="addadult"><strong>+</strong></button>
+				    <input type="text" class="form-control col-2" id="adultTickets" placeholder="${this.adult}">
+				    <button type="button" class="btn btn-secondary removebtn" id="removeadult"><strong>-</strong></button>
+				  </div>
+				</div>
+			  <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+				  <div class="btn-group mr-2" role="group" aria-label="pensioner group">
+				    <button type="button" class="btn btn-secondary addbtn" id="addpensioner"><strong>+</strong></button>
+				    <input type="text" class="form-control col-2" id="pensionerTickets" placeholder="${this.pensioner}" >
+				    <button type="button" class="btn btn-secondary removebtn" id="removepensioner"><strong>-</strong></button>
+				  </div>
+				</div>
+			</div>
+			
+			<div class="ml-3 mt-5">			
+							<button class="btn btn-danger book-btn">Forstätt</button>
+						<div>
+						</div>
+					</div>`;
+  	$('.ticketholder').html(html);
+	}
+
+	eventHandler() {
+		JSON._load('booking').then((data) => {
+	      // Retrieve the app from JSON
+	      this.bookedSeats = data;
+	      console.log(this.bookedSeats)
+	    })
+	 
+		this.booking = {};
+		let that = this;
+		$(document).on('click','.book-btn',function(){
+			
+			that.booking.show = that.showObject;
+
+			that.booking.show.userID = that.userID;
+			that.booking.show.bookedSeats = [];
+
+			console.log(that.booking);
+			$('.seat.booked').each(function(){
+				let seat = $(this);
+				let seatID = seat.data('seatid');
+				console.log(seatID);
+				that.booking.show.bookedSeats.push(seatID);
+			});
+			 that.bookedSeats.push(that.booking);
+			//that.bookedSeats.push(that.bookedSeats);
+			console.log(that.bookedSeats)
+			//console.log(that.booking.show)
+			//Object.assign(that.booking.show, {bookedSeats: that.booking})
+			//Save booked-info + sittplats to JSON
+			JSON._save('booking', that.bookedSeats);
+			console.log('saving', that.bookedSeats)
+
+		});
+
+		// JSON._load('booking',(data){
+		// 	this.bookings = data;
+		// 	this.seatOccupied();
+		// });
+
+	$(document).on('click', '.addbtn .removebtn', function() {
+		if ($('.addbtn') && $('#addchild')) {
+			this.child = this.child+1;
+		}
+		else if ($('.addbtn') && $('#addadult')) {
+			this.adult = this.adult+1;
+		}
+		else if ($('.addbtn') && $('#addpensioner')) {
+			this.pensioner = this.pensioner+1;
+		}
+		else if ($('.addbtn') && $('#removechild')) {
+			this.child = this.child-1;
+		}
+		else if ($('.addbtn') && $('#removeadult')) {
+			this.adult = this.adult-1;
+		}
+		else if ($('.addbtn') && $('#removepensioner')) {
+			this.pensioner = this.pensioner-1;
+		}
+		else { 
+			console.log('no changes');
+		}
+		renderTicketButtons();
+	});
+	
+		
+	} // end eventhandler
+
+
+
+} // end class
