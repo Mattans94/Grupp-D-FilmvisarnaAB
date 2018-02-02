5a3d8122b72a1dff74f3515830b1de46c762a85f
Johan <jmiklo@outlook.com>
2018-02-02 10:47:48 +0100

    extended base

--- www/js/classes/booking.class.js
+++ www/js/classes/booking.class.js
@@ -1,14 +1,19 @@
-class Booking{
+class Booking extends Base{
 
 	// constructor(userid, date, time, auditorium) {
 	// 
 
 
 	constructor() {
+		super();
 		this.date = '2018-03-04';
 		this.time = '21.00';
 		this.auditorium = 'Lilla Salongen';
 		this.userID = 1;
+
+		this.child=0;
+		this.adult=0;
+		this.pensioner=0;
 	//Alla beställningar som har gjorts
 		JSON._load('booking').then((seats) => {
 	      // Retrieve the app from JSON
@@ -28,6 +33,8 @@ class Booking{
 	start(){
 		this.getshowObject('Wind River', this.auditorium, this.date, this.time);
 	    this.eventHandler();
+	    this.renderTicketButtons();
+	    theater.scale();
 		
 	}
 
@@ -39,6 +46,39 @@ class Booking{
 		this.showObject = this.showObject.find((x) => time == x.time);
 		// this.showObject = this.showObject[0]
 		console.log(this.showObject);
+	}
+
+	renderTicketButtons() {
+		let html = `<div class="row justify-content-center">
+			  <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+				  <div class="btn-group mr-2" role="group" aria-label="child group">
+				    <button type="button" class="btn btn-secondary adddbtn" id="addchild"><strong>+</strong></button>
+				    <input type="text" class="form-control col-2" id="childTickets" placeholder="${this.child}">
+				    <button type="button" class="btn btn-secondary removebtn" id="removechild"><strong>-</strong></button>
+				  </div>
+				</div>
+			  <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+				  <div class="btn-group mr-2" role="group" aria-label="adult group">
+				    <button type="button" class="btn btn-secondary addbtn" id="addadult"><strong>+</strong></button>
+				    <input type="text" class="form-control col-2" id="adultTickets" placeholder="${this.adult}">
+				    <button type="button" class="btn btn-secondary removebtn" id="removeadult"><strong>-</strong></button>
+				  </div>
+				</div>
+			  <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+				  <div class="btn-group mr-2" role="group" aria-label="pensioner group">
+				    <button type="button" class="btn btn-secondary addbtn" id="addpensioner"><strong>+</strong></button>
+				    <input type="text" class="form-control col-2" id="pensionerTickets" placeholder="${this.pensioner}" >
+				    <button type="button" class="btn btn-secondary removebtn" id="removepensioner"><strong>-</strong></button>
+				  </div>
+				</div>
+			</div>
+			
+			<div class="ml-3 mt-5">			
+							<button class="btn btn-danger book-btn">Forstätt</button>
+						<div>
+						</div>
+					</div>`;
+  	$('.ticketholder').html(html);
 	}
 
 	eventHandler() {
@@ -80,7 +120,7 @@ class Booking{
 		// 	this.seatOccupied();
 		// });
 
-			$(document).on("click", '.addbtn .removebtn', function() {
+	$(document).on("click", '.addbtn .removebtn', function() {
 		if ($('.addbtn') && $('#addchild')) {
 			this.child = this.child+1;
 		}
@@ -102,8 +142,12 @@ class Booking{
 		else { 
 			console.log('no changes');
 		}
+		// renderTicketButtons();
 	});
-	}
+	
 		
 	} // end eventhandler
+
+
+
 } // end class
