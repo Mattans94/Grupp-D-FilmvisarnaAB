e551422f6172ac0365ca033bf741fb6901a831bf
Unknown <andreas@stickydirt.se>
2018-02-12 11:45:25 +0100

    Merge branch 'develop' into feature/seatstotalallowed

--- www/js/classes/booking.class.js
+++ www/js/classes/booking.class.js
@@ -132,8 +132,11 @@ class Booking extends Base{
 			bookedSeats.push(seatID);
 		});
 		// Skicka in allt modalen behöver veta
+
+		this.prices.calculateTotalPrice()
+		$('#modalInputContainer').empty();
     let bookingModal = new BookingModal(this.showObject, this.prices.totalPrice, bookedSeats);
-    $('main').append(bookingModal.template());
+    $('#modalInputContainer').append(bookingModal.template());
 		$('#bookingConfirmationModalToggler').trigger('click');
   }
 	// Lång eventHandler - kanske behövs.
