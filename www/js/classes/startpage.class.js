ff5e1bc942498d85eed9cc711980904802313356
Unknown <paulina.hertor@gmail.com>
2018-02-14 15:31:32 +0100

    Small CSS-fix

--- www/js/classes/startpage.class.js
+++ www/js/classes/startpage.class.js
@@ -8,7 +8,7 @@ class StartPage extends Base {
 		return `
 		<div class="movieBlock mb-3 mr-2 ml-2 p-3 d-flex flex-column col">
 			<a href="${movieObject.title}" class="pop"><img class="img-fluid" src="/img/posters/${movieObject.images[0]}"></a>
-			<h5 class="mt-2 mb-2">${movieObject.title}</h5>
+			<a href="${movieObject.title}" class="pop"><h5 class="mt-2 mb-2">${movieObject.title}</h5></a>
 
 		</div>`
 	}
