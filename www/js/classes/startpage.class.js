db15c78d051dc4b38517fd216e0c183bb4dc4ecc
Unknown <paulina.hertor@gmail.com>
2018-02-14 13:33:41 +0100

    link to imgs on startpage

--- www/js/classes/startpage.class.js
+++ www/js/classes/startpage.class.js
@@ -7,9 +7,8 @@ class StartPage extends Base {
 		let movieObject = this.getMovieObject(movieTitle);
 		return `
 		<div class="movieBlock mb-3 mr-2 ml-2 p-3 d-flex flex-column col">
-			<img class="img-fluid" src="/img/posters/${movieObject.images[0]}">
-
-			<h4 class="mt-2 mb-2">${movieObject.title}</h4>
+			<a href="${movieObject.title}" class="pop"><img class="img-fluid" src="/img/posters/${movieObject.images[0]}"></a>
+			<h5 class="mt-2 mb-2">${movieObject.title}</h5>
 			<a href="${movieObject.title}" class="pop align-self-end mt-auto"><button type="button" class="btn btn-danger showmorebtn">Visa mer!</button></a>
 
 		</div>`
