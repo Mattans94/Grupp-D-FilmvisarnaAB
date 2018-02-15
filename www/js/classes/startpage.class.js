6d4aec651af24747782e3e505586ec43aec29730
Unknown <andreas@stickydirt.se>
2018-02-15 10:21:17 +0100

    Removed title from startpage

--- www/js/classes/startpage.class.js
+++ www/js/classes/startpage.class.js
@@ -10,7 +10,6 @@ class StartPage extends Base {
 		<div class="movieBlock mb-3 mr-2 ml-2 p-3 col">
 			<a href="${movieObject.title}" class="pop d-flex flex-column">
 				<img class="img-fluid" src="/img/posters/${movieObject.images[0]}">
-				<h5 class="mt-2 mb-2 align-self-center">${movieObject.title}</h5>
 			</a>
 		</div>
 		`
