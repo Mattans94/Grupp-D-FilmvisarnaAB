23e5a9c715fd6515b7bbb50f8437c1a91fa1798d
Emil <emil_emil94@hotmail.com>
2018-02-15 15:54:46 +0100

    Animations on startpage movies

--- www/js/classes/startpage.class.js
+++ www/js/classes/startpage.class.js
@@ -6,9 +6,10 @@ class StartPage extends Base {
 	renderMovie(movieTitle){
 		let movieObject = this.getMovieObject(movieTitle);
 		return `
-		<div class="movieBlock mb-3 mr-2 ml-2 p-3 col">
+		<div class="movieBlock mb-3 mr-2 ml-2 p-3 col" id="wrapper">
 			<a href="${movieObject.title}" class="pop d-flex flex-column">
-				<img class="img-fluid" src="/img/posters/${movieObject.images[0]}">
+				<img class="img-fluid animation" src="/img/posters/${movieObject.images[0]}">
+					<h5 class="wrap-text m-0 p-0">${movieObject.title}</h5>	
 			</a>
 		</div>
 		`
